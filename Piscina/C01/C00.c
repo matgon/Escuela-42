82db5b1707fc4e3cd1cc4a145fc6724c8bb1d08b
Mateo González <95885267+matgon@users.noreply.github.com>
2025-02-24 10:10:58 +0100

    ft_ft corregido

--- Piscina/C01/C00.c
+++ Piscina/C01/C00.c
@@ -1,17 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void ft_ft(int *nbr){
+void	ft_ft(int *nbr)
+{
     *nbr = 42;
-}
-
-int main(){
-    int *ptr = (int*) malloc(sizeof(int));
-
-    ft_ft(ptr);
-    printf("%d\n", *ptr);
-
-    free(ptr);
-
-    return 0;
 }
