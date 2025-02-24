c4a0c2fc0e6a2ca7929d85dfbf999859df212770
Mateo <matglezdemi@gmail.com>
2025-02-24 21:54:19 +0100

    ejercicios C01 preparados

---
+++ Piscina/C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	temp;
+	int	i;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		temp = tab[i];
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = temp;
+		i++;
+	}
+}
