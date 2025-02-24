09dd748660641cdd882da3061ecc4bf0251730a1
Mateo González <95885267+matgon@users.noreply.github.com>
2025-02-24 10:18:25 +0100

    Update C07.c

--- Piscina/C01/C07.c
+++ Piscina/C01/C07.c
@@ -3,10 +3,14 @@
 
 void ft_rev_int_tab(int *tab, int size) {
     int temp, i;
-    for (i = 0; i < size / 2; i++) {
+
+    i = 0;
+    while(i < size / 2)
+    {        
         temp = tab[i];
         tab[i] = tab[size - 1 - i];
         tab[size - 1 - i] = temp;
+        i++;
     }
 }
 
