fc5f438626703b5d73a54903f3303793d91d6a93
Mateo <matglezdemi@gmail.com>
2025-04-13 12:15:13 +0200

    main del map borrado

--- Common_core/libft/ft_lstmap_bonus.c
+++ Common_core/libft/ft_lstmap_bonus.c
@@ -6,47 +6,11 @@
 /*   By: matgonza <matgonza@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 22:58:56 by matgonza          #+#    #+#             */
-/*   Updated: 2025/04/13 11:51:54 by matgonza         ###   ########.fr       */
+/*   Updated: 2025/04/13 12:14:57 by matgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// t_list	*ft_lstnew(void *content)
-// {
-// 	t_list	*new;
-
-// 	new = (t_list *) malloc(sizeof(t_list));
-// 	if (!new)
-// 		return (NULL);
-// 	if (!content)
-// 		new->content = NULL;
-// 	else
-// 		new->content = content;
-// 	new->next = NULL;
-// 	return (new);
-// }
-
-// void	ft_lstdelone(t_list *lst, void (*del)(void*))
-// {
-// 	del(lst->content);
-// 	free(lst);
-// }
-
-// void	ft_lstclear(t_list **lst, void (*del)(void*))
-// {
-// 	t_list	*aux1;
-// 	t_list	*aux2;
-
-// 	aux1 = *lst;
-// 	while (aux1 != NULL)
-// 	{
-// 		aux2 = aux1->next;
-// 		ft_lstdelone(aux1, del);
-// 		aux1 = aux2;
-// 	}
-// 	*lst = NULL;
-// }
 
 /// @brief Itera la lista ’lst’ y aplica la función ’f’ al
 /// contenido de cada nodo. Crea una lista resultante
@@ -88,90 +52,3 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (ret);
 }
-
-
-
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include "libft.h"  // Asegúrate de incluir tu header
-
-// t_list	*ft_lstlast(t_list *lst)
-// {
-// 	while (lst != NULL)
-// 	{
-// 		if (lst->next == NULL)
-// 			return (lst);
-// 		lst = lst->next;
-// 	}
-// 	return (NULL);
-// }
-
-// void	ft_lstadd_back(t_list **lst, t_list *new)
-// {
-// 	t_list	*last;
-
-// 	last = ft_lstlast(*lst);
-// 	if (last == NULL)
-// 		*lst = new;
-// 	else
-// 		last->next = new;
-// }
-
-// // Función que transforma el contenido (duplica string y lo pasa a mayúsculas)
-// void *map_to_upper(void *content)
-// {
-//     char *str = (char *)content;
-//     char *new_str = strdup(str);  // Reservamos nuevo string
-//     if (!new_str)
-//         return NULL;
-
-//     for (int i = 0; new_str[i]; i++)
-//         if (new_str[i] >= 'a' && new_str[i] <= 'z')
-//             new_str[i] -= 32;  // Convertimos a mayúscula
-
-//     return (void *)new_str;
-// }
-
-// // Función para liberar memoria de un nodo
-// void del_content(void *content)
-// {
-//     free(content);
-// }
-
-// // Función para imprimir una lista
-// void print_list(t_list *lst)
-// {
-//     while (lst)
-//     {
-//         printf("%s -> ", (char *)lst->content);
-//         lst = lst->next;
-//     }
-//     printf("NULL\n");
-// }
-// void *f_fail(void *c) { (void)c; return NULL; }
-
-// int main(void)
-// {
-//     t_list *n1 = ft_lstnew(strdup("que"));
-//     // t_list *n2 = ft_lstnew(strdup("que"));
-//     // t_list *n3 = ft_lstnew(strdup("tal"));
-
-//     // n1->next = n2;
-//     // n2->next = n3;
-
-//     printf("Lista original:\n");
-//     print_list(n1);
-
-//     t_list *mapped = ft_lstmap(n1, &f_fail, &del_content);
-
-//     printf("\nLista mapeada:\n");
-//     print_list(mapped);
-
-//     // Liberar memoria
-//     ft_lstclear(&n1, &del_content);
-//     ft_lstclear(&mapped, &del_content);
-
-//     return 0;
-// }
