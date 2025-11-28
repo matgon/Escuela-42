bdf48fb1377a41eb8d971cd7e7543b02b87fb464
Mateo <matglezdemi@gmail.com>
2025-11-28 20:23:35 +0100

    pipex parte basica terminada

--- common_core/pipex/pipex.c
+++ common_core/pipex/pipex.c
@@ -1,66 +1,132 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matgonza <matgonza@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 18:29:27 by matgonza          #+#    #+#             */
+/*   Updated: 2025/11/28 19:29:20 by matgonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex.h"
 
-#include <stdio.h>
-
-char *find_cmd_path(const char *cmd)
+void	error(char *msg, int status)
 {
-    char *path;
+	perror(msg);
+	exit(status);
+}
 
+void	free_split(char **strs)
+{
+	int	i;
 
-    return (path);
+	if (!strs)
+		return;
+	i = 0;
+	while (strs[i])
+		free(strs[i++]);
+	free(strs);
+}
+
+char	*find_cmd_path(const char *cmd, char **envp)
+{
+	int		i;
+	char	*path;
+	char	*aux;
+	char	**paths_in_path;
+
+	i = 0;
+	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5))
+		i++;
+	if (!envp[i])
+		return (NULL);
+	paths_in_path = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (paths_in_path[i])
+	{
+		aux = ft_strjoin(paths_in_path[i], "/"); //aqui no se esta liberando el primer join
+		path = ft_strjoin(aux, cmd);
+		free(aux);
+		if (access(path, X_OK) == 0) //devuelve 0 cuando esta bien
+		{
+			free_split(paths_in_path);
+			return (path);
+		}
+		free(path);
+		i++;
+	}
+	free_split(paths_in_path);
+	return (NULL);
 }
 
 int main(int argc, char **argv, char **envp)
 {
-    pid_t pid1, pid2, status;
-    int fd_in, fd_out;
-    int p[2];
-    char **cmd;
+	pid_t pid1, pid2, status;
+	int fd_in, fd_out;
+	int p[2];
+	char **cmd;
+	char *path;
 
-    
-    if (pipe(p)) //-1 error, 0 bien
-    {
-        perror("Error en la creacion de la pipe.");
-        exit(1);
-    }
-    cmd = ft_split(argv[2], ' ');
-    execve(cmd[0], cmd, envp);
-    perror("Error en execve");
-    printf("comando completo 1: %s", argv[2]);
-    printf("comando completo 2: %s", argv[3]);
-    // pid1 = fork(); // primer hijo
-    // if (pid1 == 0)
-    // {
-    //     fd_in = open(argv[1], O_RDONLY); //fichero de entrada
-    //     dup2(fd_in, 0); //ahora la entrada va a ser lo que hay en el fichero
-    //     dup2(p[1], 1); //ahora la salida va a escribir en el pipe
-    //     close(fd_in);
-    //     close(p[0]);  //cierro el pipe de lectura ya que no lo necesito en este hijo
-    //     close(p[1]);
-    //     cmd = ft_split(argv[2], ' ');
-    //     execve(cmd[0], cmd, envp); //ejecuto el comando y sale por p[1]
-    //     perror("Error en execve 1.");
-    //     exit(1);
-    // }
-    // pid2 = fork(); //segundo hijo
-    // if (pid2 == 0)
-    // {
-    //     fd_out = open(argv[argc-1], O_CREAT | O_WRONLY | O_TRUNC, 0644); //abro el fichero de escritura
-    //     dup2(p[0], 0); //ahora el stdin va a apuntar al pipe de lectura
-    //     dup2(fd_out, 1); //ahora la stdout va a apuntar al fichero de salida
-    //     close(fd_out);
-    //     close(p[0]);
-    //     close(p[1]); //cierro el pipe de escritura, ya que no voy a escribir nada
-    //     cmd = ft_split(argv[3], ' ');
-    //     printf("comando completo 2: %s", argv[3]);
-    //     execve(cmd[0], cmd, envp);
-    //     perror("Error en execve 2.");
-    //     exit(1);
-    // }
-    // waitpid(pid1, NULL, 0);
-    // waitpid(pid2, &status, 0);
-    close(p[0]);
-    close(p[1]);
-    return 0;
-    // return (status >> 8);
+	if (pipe(p))
+	{
+		perror("Error en la creacion de la pipe.");
+		exit(1);
+	}
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		fd_in = open(argv[1], O_RDONLY);
+		if (fd_in == -1)
+			error("Failed opening file 1.", 1);
+		if (dup2(fd_in, STDIN_FILENO) == -1 || dup2(p[1], STDOUT_FILENO) == -1)
+			error("Failed duplicating descriptors.", 1);
+		close(fd_in);
+		close(p[0]);
+		close(p[1]);
+		cmd = ft_split(argv[2], ' ');
+		if (!cmd)
+			error("Failed allocating in split.", 1);
+		path = find_cmd_path(cmd[0], envp);
+		if (!path)
+		{
+			free_split(cmd);
+			error("Command not found", 1);
+		}
+		execve(path, cmd, envp);
+		free_split(cmd);
+		free(path);
+		error("Failed in execve 1", 1);
+	}
+	pid2 = fork();
+	if (pid2 == 0)
+	{
+		fd_out = open(argv[argc-1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		if (fd_out == -1)
+			error("Failed opening file 2.", 1);
+		if (dup2(p[0], STDIN_FILENO == -1 || dup2(fd_out, STDOUT_FILENO) == -1))
+			error("Failed duplicating descriptors.", 1);
+		close(fd_out);
+		close(p[0]);
+		close(p[1]);
+		cmd = ft_split(argv[3], ' ');
+		if (!cmd)
+			error("Failed allocating in split.", 1);
+		path = find_cmd_path(cmd[0], envp);
+		if (!path)
+		{
+			free_split(cmd);
+			error("Command not found.", 127);
+		}
+		execve(path, cmd, envp);
+		free_split(cmd);
+		free(path);
+		error("Failed in execve 2.", 1);
+	}
+	close(p[0]);
+	close(p[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, &status, 0);
+	return (status >> 8);
 }
--- common_core/pipex/pipex.h
+++ common_core/pipex/pipex.h
@@ -1,10 +1,22 @@
-#ifndef PIPEX_H
-#define PIPEX_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matgonza <matgonza@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 19:34:21 by matgonza          #+#    #+#             */
+/*   Updated: 2025/11/28 19:34:59 by matgonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include "libft/libft.h"
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include "libft/libft.h"
 
 #endif
